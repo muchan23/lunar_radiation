a55ae32041cab2fb6b98c6f84f844bb4d3f01f62
muchan23 <murakami-yuki866@g.ecc.u-tokyo.ac.jp>
2024-11-12 16:29:35 +0900

    first commit

---
+++ construction.cc
@@ -0,0 +1,283 @@
+
+#include "construction.hh"
+#include "G4VisAttributes.hh"
+
+MyDetectorConstruction::MyDetectorConstruction()
+{
+    nCols = 10;
+    nRows = 10;
+
+    fMessenger = new G4GenericMessenger(this, "/detector/", "Detector Construction");
+
+    fMessenger->DeclareProperty("nCols", nCols, "Number of cols");
+    fMessenger->DeclareProperty("nRows", nRows, "Number of rows");
+    fMessenger->DeclareProperty("cherenkov", isCherenkov, "Construct Cherenkov detector");
+    fMessenger->DeclareProperty("scintillator", isScintillator, "Construct Scintillator");
+    fMessenger->DeclareProperty("atmosphere", isScintillator, "Construct Atmosphere");
+
+    DefineMaterials();
+
+    isCherenkov = false;
+    isScintillator = false;
+    isTOF = false;
+    isAtmosphere = false;
+    isMoon=true;
+
+    xWorld = 500.*km;
+    yWorld = 500.*km;
+    zWorld = 500.*km;
+}
+
+MyDetectorConstruction::~MyDetectorConstruction()
+{}
+
+void MyDetectorConstruction::DefineMaterials()
+{
+    G4NistManager *nist = G4NistManager::Instance();
+
+    C = nist->FindOrBuildElement("C");
+    SiO2 = new G4Material("SiO2", 2.201*g/cm3, 2);
+    Aerogel = new G4Material("Aerogel", 0.200*g/cm3, 3);
+    worldMat = nist->FindOrBuildMaterial("G4_AIR");
+    H2O = new G4Material("H2O", 1.000*g/cm3, 2);
+
+    SiO2->AddElement(nist->FindOrBuildElement("Si"), 1);
+    SiO2->AddElement(nist->FindOrBuildElement("O"), 2);
+
+    H2O->AddElement(nist->FindOrBuildElement("H"), 2);
+    H2O->AddElement(nist->FindOrBuildElement("O"), 1);
+
+    Aerogel->AddMaterial(SiO2, 62.5*perCent);
+    Aerogel->AddMaterial(H2O, 37.4*perCent);
+    Aerogel->AddElement(C, 0.1*perCent);
+
+    G4double energy[2] = {1.239841939*eV/0.9, 1.239841939*eV/0.2};
+    G4double rindexAerogel[2] = {1.1, 1.1};
+    G4double rindexWorld[2] = {1.0, 1.0};
+    G4double rindexNaI[2] = {1.78, 1.78};
+
+    G4MaterialPropertiesTable *mptAerogel = new G4MaterialPropertiesTable();
+    mptAerogel->AddProperty("RINDEX", energy, rindexAerogel, 2);
+
+    Aerogel->SetMaterialPropertiesTable(mptAerogel);
+
+    G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
+    mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
+
+    Na = nist->FindOrBuildElement("Na");
+    I = nist->FindOrBuildElement("I");
+    NaI = new G4Material("NaI", 3.67*g/cm3, 2);
+    NaI->AddElement(Na, 1);
+    NaI->AddElement(I, 1);
+
+    G4double fraction[2] = {1.0, 1.0};
+
+    G4MaterialPropertiesTable *mptNaI = new G4MaterialPropertiesTable();
+    mptNaI->AddProperty("RINDEX", energy, rindexNaI, 2);
+    mptNaI->AddProperty("SCINTILLATIONCOMPONENT1", energy, fraction, 2);
+    mptNaI->AddConstProperty("SCINTILLATIONYIELD",38./keV);
+    mptNaI->AddConstProperty("RESOLUTIONSCALE",1.0);
+    mptNaI->AddConstProperty("SCINTILLATIONTIMECONSTANT1", 250.*ns);
+    mptNaI->AddConstProperty("SCINTILLATIONYIELD1",1.);
+    NaI->SetMaterialPropertiesTable(mptNaI);
+
+    worldMat->SetMaterialPropertiesTable(mptWorld);
+
+    mirrorSurface = new G4OpticalSurface("mirrorSurface");
+
+    mirrorSurface->SetType(dielectric_metal);
+    mirrorSurface->SetFinish(ground);
+    mirrorSurface->SetModel(unified);
+
+    G4double reflectivity[2] = {0.9, 0.9};
+
+    G4MaterialPropertiesTable *mptMirror = new G4MaterialPropertiesTable();
+    mptMirror->AddProperty("REFLECTIVITY", energy, reflectivity, 2);
+
+    mirrorSurface->SetMaterialPropertiesTable(mptMirror);
+
+    G4double density0 = 1.29*kg/m3;
+    G4double aN = 14.01*g/mole;
+    G4double aO = 16.00*g/mole;
+
+    G4Element *elN = new G4Element("Nitrogen", "N", 7, aN);
+    G4Element *elO = new G4Element("Oxygen", "O", 8, aO);
+
+    G4double f = 3;
+    G4double R = 8.3144626181532;
+    G4double g0 = 9.81;
+    G4double kappa = (f+2)/f;
+    G4double T = 293.15;
+    G4double M = (0.3*14.01 + 0.7*16.0)/1000.;
+
+    for(G4int i = 0; i < 10; i++)
+    {
+        std::stringstream stri;
+        stri << i;
+        G4double h = 40e3/10.*i;
+        G4double density = density0*pow((1-(kappa-1)/kappa*M*g0*h/(R*T)),(1/(kappa-1)));
+        Air[i] = new G4Material("G4_AIR_"+stri.str(), density, 2);
+        Air[i]->AddElement(elN, 70*perCent);
+        Air[i]->AddElement(elO, 30*perCent);
+    }
+
+    //月のレゴリスの構成を定義
+    lunarRegolith = new G4Material("LunarRegolith", 1.5*g/cm3, 3);
+    lunarRegolith->AddElement(nist->FindOrBuildElement("Si"), 50*perCent);
+    lunarRegolith->AddElement(nist->FindOrBuildElement("O"), 40*perCent);
+    lunarRegolith->AddElement(nist->FindOrBuildElement("Fe"), 10*perCent);
+
+    G4MaterialPropertiesTable *mptLunarRegolith = new G4MaterialPropertiesTable();
+    mptLunarRegolith->AddProperty("REFLECTIVITY", energy, reflectivity, 2);
+
+    // 透過率の設定（例として0.1とする）
+    G4double transmission[2] = {0.2, 0.2}; // 透過率（20%）
+    mptLunarRegolith->AddProperty("TRANSMITTANCE", energy, transmission, 2);
+
+    lunarRegolith->SetMaterialPropertiesTable(mptLunarRegolith);
+}
+
+//月の地面
+void MyDetectorConstruction::ConstructGround()
+{
+    G4double groundThickness = 450.0 * km; // 地面の厚さ
+    solidGround = new G4Box("solidGround", 600.0 * km, 600.0 * km, groundThickness / 2.0);
+    logicGround = new G4LogicalVolume(solidGround, lunarRegolith, "logicGround");
+
+    // 月面の層に色を設定
+    G4VisAttributes *visAttributes = new G4VisAttributes(G4Colour(0.9, 0.9, 0.9)); // グレー色
+    logicGround->SetVisAttributes(visAttributes);
+
+    // 光学表面の設定
+    G4OpticalSurface *mirrorSurface = new G4OpticalSurface("mirrorSurface");
+    mirrorSurface->SetType(dielectric_metal);
+    mirrorSurface->SetFinish(polished);
+    mirrorSurface->SetModel(unified);
+
+    // エネルギー範囲と反射率を宣言
+    G4double energy[2] = {0.1*eV, 10.0*eV}; // エネルギー範囲
+    G4double reflectivity[2] = {0.9, 0.9}; // 反射率（90%）
+
+    // 光学表面に反射率を設定
+    G4MaterialPropertiesTable *mptMirrorSurface = new G4MaterialPropertiesTable();
+    mptMirrorSurface->AddProperty("REFLECTIVITY", energy, reflectivity, 2);
+    mirrorSurface->SetMaterialPropertiesTable(mptMirrorSurface);
+
+    G4LogicalSkinSurface *skin = new G4LogicalSkinSurface("skin", logicGround, mirrorSurface);
+
+    // 地面をワールド座標系に配置
+    physGround = new G4PVPlacement(0, G4ThreeVector(0, 0, -groundThickness / 2.0), logicGround, "physGround", logicWorld, false, 0, true);
+}
+
+
+void MyDetectorConstruction::ConstructScintillator()
+{
+    solidScintillator = new G4Box("solidScintillator", 5*cm, 5*cm, 6*cm);
+
+    logicScintillator = new G4LogicalVolume(solidScintillator, NaI, "logicalScintillator");
+
+    G4LogicalSkinSurface *skin = new G4LogicalSkinSurface("skin", logicWorld, mirrorSurface);
+
+    fScoringVolume = logicScintillator;
+
+    solidDetector = new G4Box("solidDetector", 1.*cm, 5.*cm, 6*cm);
+
+    logicDetector = new G4LogicalVolume(solidDetector, worldMat, "logicDetector");
+
+    for(G4int i = 0; i < 6; i++)
+    {
+        for(G4int j = 0; j < 16; j++)
+        {
+            G4Rotate3D rotZ(j*22.5*deg, G4ThreeVector(0,0,1));
+            G4Translate3D transXScint(G4ThreeVector(5./tan(22.5/2*deg)*cm+5.*cm, 0.*cm, -40.*cm + i*15*cm));
+            G4Transform3D transformScint = (rotZ)*(transXScint);
+
+            G4Translate3D transXDet(G4ThreeVector(5./tan(22.5/2*deg)*cm+5.*cm+6.*cm, 0.*cm, -40.*cm + i*15*cm));
+            G4Transform3D transformDet = (rotZ)*(transXDet);
+
+            physScintillator = new G4PVPlacement(transformScint, logicScintillator, "physScintillator", logicWorld, false, 0, true);
+
+            physDetector = new G4PVPlacement(transformDet, logicDetector, "physDetector", logicWorld, false, 0, true);
+        }
+    }
+}
+
+void MyDetectorConstruction::ConstructCherenkov()
+{
+    solidRadiator = new G4Box("solidRadiator", 0.4*m, 0.4*m, 0.01*m);
+
+    logicRadiator = new G4LogicalVolume(solidRadiator, Aerogel, "logicalRadiator");
+
+    fScoringVolume = logicRadiator;
+
+    physRadiator = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.25*m), logicRadiator, "physRadiator", logicWorld, false, 0, true);
+
+    solidDetector = new G4Box("solidDetector", xWorld/nRows, xWorld/nCols, 0.01*m);
+
+    logicDetector = new G4LogicalVolume(solidDetector, worldMat, "logicDetector");
+
+    for(G4int i = 0; i < nRows; i++)
+    {
+        for(G4int j = 0; j < nCols; j++)
+        {
+            physDetector = new G4PVPlacement(0, G4ThreeVector(-0.5*m+(i+0.5)*m/nRows, -0.5*m+(j+0.5)*m/nCols, 0.49*m), logicDetector, "physDetector", logicWorld, false, j+i*nCols, true);
+        }
+    }
+}
+
+void MyDetectorConstruction::ConstructTOF()
+{
+    solidDetector = new G4Box("solidDetector", 1.*m, 1.*m, 0.1*m);
+
+    logicDetector = new G4LogicalVolume(solidDetector, worldMat, "logicDetector");
+
+    physDetector = new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, -4.*m), logicDetector, "physDetector", logicWorld, false, 0, true);
+
+    physDetector = new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 3.*m), logicDetector, "physDetector", logicWorld, false, 1, true);
+}
+
+void MyDetectorConstruction::ConstructAtmosphere()
+{
+    solidAir = new G4Box("solidAir", xWorld, yWorld, zWorld/10.);
+
+    for(G4int i = 0; i < 10; i++)
+    {
+        logicAir[i] = new G4LogicalVolume(solidAir, Air[i], "logicAir");
+
+        physAir[i] = new G4PVPlacement(0, G4ThreeVector(0, 0, zWorld/10.*2*i - zWorld + zWorld/10.), logicAir[i], "physAir", logicWorld, false, i, true);
+    }
+}
+
+G4VPhysicalVolume *MyDetectorConstruction::Construct()
+{
+    solidWorld = new G4Box("solidWorld", xWorld, yWorld, zWorld);
+
+    logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
+
+    physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true);
+
+    if(isMoon)
+        ConstructGround(); // 地面の構築を呼び出す
+
+    if(isCherenkov)
+        ConstructCherenkov();
+
+    if(isScintillator)
+        ConstructScintillator();
+
+    if(isTOF)
+        ConstructTOF();
+
+    if(isAtmosphere)
+        ConstructAtmosphere();
+
+    return physWorld;
+}
+
+void MyDetectorConstruction::ConstructSDandField()
+{
+    MySensitiveDetector *sensDet = new MySensitiveDetector("SensitiveDetector");
+
+    if(logicDetector != NULL)
+        logicDetector->SetSensitiveDetector(sensDet);
+}
---
+++ construction.hh
@@ -0,0 +1,61 @@
+#ifndef CONSTRUCTION_HH
+#define CONSTRUCTION_HH
+
+#include "G4VUserDetectorConstruction.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Box.hh"
+#include "G4Tubs.hh"
+#include "G4PVPlacement.hh"
+#include "G4NistManager.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4GenericMessenger.hh"
+#include "G4OpticalSurface.hh"
+#include "G4LogicalBorderSurface.hh"
+#include "G4LogicalSkinSurface.hh"
+
+#include "detector.hh"
+
+class MyDetectorConstruction : public G4VUserDetectorConstruction
+{
+public:
+    MyDetectorConstruction();
+    ~MyDetectorConstruction();
+
+    G4LogicalVolume *GetScoringVolume() const { return fScoringVolume; }
+
+    virtual G4VPhysicalVolume *Construct();
+    void ConstructCherenkov();
+    void ConstructScintillator();
+    void ConstructTOF();
+    void ConstructAtmosphere();
+    void ConstructGround();
+
+private:
+    G4Box *solidWorld, *solidRadiator, *solidDetector, *solidScintillator, *solidAir, *solidGround;
+    G4LogicalVolume *logicWorld, *logicRadiator, *logicDetector, *logicScintillator, *logicAir[10], *logicGround;
+    G4VPhysicalVolume *physWorld, *physDetector, *physRadiator, *physScintillator, *physAir[10];
+
+    G4OpticalSurface *mirrorSurface;
+
+    G4Material *SiO2, *H2O, *Aerogel, *worldMat, *NaI, *Air[10], *lunarRegolith;
+    G4Element *C, *Na, *I;
+
+    // 月面
+    G4PVPlacement *physGround;
+
+    void DefineMaterials();
+    virtual void ConstructSDandField();
+
+    G4GenericMessenger *fMessenger;
+
+    G4LogicalVolume *fScoringVolume;
+
+    G4int nRows, nCols;
+
+    G4double xWorld, yWorld, zWorld;
+
+    G4bool isCherenkov, isScintillator, isTOF, isAtmosphere, isMoon;
+};
+
+#endif
